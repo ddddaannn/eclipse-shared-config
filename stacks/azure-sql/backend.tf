0a21d1ad6a9e75724494c13c7deaba64b5d43648
Dann Arvi Moredo <dann.moredo@3cloudsolutions.com>
2025-08-12 01:04:49 -0700

    Added backend

--- stacks/azure-sql/backend.tf
+++ stacks/azure-sql/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "rg-dann-tfstate-dev"
+    storage_account_name = "satfstate8825"
+    container_name       = "devtfstate"
+    key                  = "hubspoke-dev.tfstate"
+  }
 }
